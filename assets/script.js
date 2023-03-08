14125592bf7b78727ac5237c889f6a78f8333209
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-03-07 18:44:44 -0700

    updated code to style, html and java

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
-const quizbttn = document.getElementById('#quizstart')
-const timerleft = document.getElementById('#timestamp')
-
+const quizbttn = document.getElementById('start')
+const timerleft = document.getElementById('time')
+const startquiz = document.getElementById("startquiz")
+const quizSection = document.getElementById('quizsect')
+const question = document.getElementById('question')
+const anwserA = document.getElementById('answerA')
+const anwserB = document.getElementById('answerB')
+const anwserC = document.getElementById('answerC')
+const anwserD = document.getElementById('answerD')
 //1. create an array of questions
 var questions = [
     {
@@ -28,22 +34,45 @@ var questions = [
     }
 ]
 //2. start button to begin quiz
+function pageload() {
+    quizSection.style = "display: none";
+}
+pageload()
 
+var questionCount = 0
+function runQuiz() {
+    timerleft.innerHTML = 'TIME: ' + sec;
+    question.innerHTML = questions[questionCount].q;
+    anwserA.innerHTML = questions[questionCount].a;
+    anwserB.innerHTML = questions[questionCount].b;
+    anwserC.innerHTML = questions[questionCount].c;
+    anwserD.innerHTML = questions[questionCount].d
+}
+
+quizbttn.addEventListener('click', function () {
+    setTime();
+    startquiz.style = "display: none";
+    quizSection.style = "display: block";
+    runQuiz()
+
+});
 //3. create timer abd start quiz
-
-function setTime () {
-    var sec = 15
-    setInterval(()=>{
-timerleft.innerHTML = sec;
-sec --;
-    }, 1000)
+var sec = 3
+function setTime() {
+    var Intervali = setInterval(function () {
+        timerleft.innerHTML = 'TIME: ' + sec;
+        sec--;
+        if (sec === 0) {
+            clearInterval(Intervali)
         }
+    }, 1000)
+}
 
 
 //4. display first question with 4 choices
+
 //5. manage user decision
 //6. end quiz and input uzer intials
 //8. save highscore in local stoarge
 //9. redirect them to highscore
 
-quizbttn.addEventListener('click', setTime);
